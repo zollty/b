dbac36d0b3fb73f59dcc794dfb13ef885cd3eaf0
zollty <zollty@163.com>
2020-08-27 23:09:18 +0800

    update site style

--- public/js/base.js
+++ public/js/base.js
@@ -9,6 +9,86 @@ function showDisqus() {
     });
 }
 
+//Handles the go to top button at the footer
+var handleGoTop = function () {
+    var offset = 300;
+    var duration = 500;
+
+    if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {  // ios supported
+        $(window).bind("touchend touchcancel touchleave", function(e){
+           if ($(this).scrollTop() > offset) {
+                $('.scroll-to-top').fadeIn(duration);
+            } else {
+                $('.scroll-to-top').fadeOut(duration);
+            }
+        });
+    } else {  // general
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.scroll-to-top').fadeIn(duration);
+            } else {
+                $('.scroll-to-top').fadeOut(duration);
+            }
+        });
+    }
+
+    $('.scroll-to-top').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+};
+
+function imgShow(outerdiv, innerdiv, bigimg, _this) {
+  var src = _this.attr("src"); //获取当前点击的pimg元素中的src属性  
+  $(bigimg).attr("src", src); //设置#bigimg元素的src属性  
+
+  /*获取当前点击图片的真实大小，并显示弹出层及大图*/
+  $("<img/>").attr("src", src).load(function() {
+      var windowW = $(window).width(); //获取当前窗口宽度  
+      var windowH = $(window).height(); //获取当前窗口高度  
+      var realWidth = this.width; //获取图片真实宽度  
+      var realHeight = this.height; //获取图片真实高度  
+      var imgWidth, imgHeight;
+      var scale = 0.96; //缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放  
+
+      if (realHeight > windowH * scale) { //判断图片高度  
+          imgHeight = windowH * scale; //如大于窗口高度，图片高度进行缩放  
+          imgWidth = imgHeight /
+              realHeight * realWidth; // 等比例缩放宽度
+          if (imgWidth > windowW * scale) { //如宽度扔大于窗口宽度  
+              imgWidth = windowW * scale; //再对宽度进行缩放  
+          }
+      } else if (realWidth > windowW * scale) { //如图片高度合适，判断图片宽度  
+          imgWidth = windowW * scale; //如大于窗口宽度，图片宽度进行缩放  
+          imgHeight = imgWidth / realWidth * realHeight; // 等比例缩放高度
+      } else { //如果图片真实高度和宽度都符合要求，高宽不变  
+          imgWidth = realWidth;
+          imgHeight = realHeight;
+      }
+      $(bigimg).css("width", imgWidth); //以最终的宽度对图片缩放  
+
+      var w = (windowW - imgWidth) / 2; // 计算图片与窗口左边距
+      var h = (windowH - imgHeight) / 2; // 计算图片与窗口上边距
+      $(innerdiv).css({
+          "top": h,
+          "left": w
+      }); //设置#innerdiv的top和left属性  
+      $(outerdiv).fadeIn("fast"); //淡入显示#outerdiv及.pimg  
+  });
+
+  $(outerdiv).click(function() { //再次点击淡出消失弹出层  
+      $(this).fadeOut("fast");
+  });
+}
+
+function handleImage() {
+	$(".img-thumbnail").click(function() {
+        var _this = $(this); //将当前的pimg元素作为_this传入函数  
+        imgShow("#outerdiv", "#innerdiv", "#bigimg", _this);
+    });
+}
+
 /* 控制导航按钮动作 */
 function nav_click() {
     if ($('#nav_btn i').hasClass('fa-angle-right')) {
@@ -112,15 +192,18 @@ function loadUE() {
     ds.type = 'text/javascript';
     ds.async = true;
     // ds.src = "/b/public/js/ueditor.parse.min.js";
-    ds.src = assets_url + "js/ueditor.parse.min.js";
+    ds.src = assets_url + "js/ueditor.parse2.min.js";
     ds.charset = 'UTF-8';
     ds.id = 'uepjs';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
 }
 
 function contentEffects(init) {
+    // 为图片添加bootstrap的样式……
+    $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
+
     // 动态进入有Disqus的页面
-    $('body').on('click', '.show-commend', showDisqus);
+    //$('body').on('click', '.show-commend', showDisqus);
 
     // 动态进入标签和分类页面
     if ($('#cate-wr').length > 0) {
@@ -142,14 +225,12 @@ function contentEffects(init) {
     //  $("#ueditor_background").remove();
     //}
 
-    // 为图片添加bootstrap的样式……（其实没什么用）
-    $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
-
     loadUE();
     loadBlockUI();
     loadTagCloud();
+    handleImage();
+    handleGoTop();
 }
-
 
 $(document).ready(function () {
 
